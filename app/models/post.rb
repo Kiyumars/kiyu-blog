02cd2f2ab2655535a6bb2a48c3711a8066f3c3e8
Kiyu <qconnection@gmail.com>
2015-02-14 20:26:52 +0100

    Validate title and content of Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true, allow_blank: false, length: { maximum: 255, message: "Title is too long" }
+	validates :content, presence: true, allow_blank: false
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-	test "reject post without title" do
+	test "reject post wit blank title" do
 		assert_no_difference "Post.count" do
 			Post.create(title: "", content: "Lorem Ipsum Est")
+		end
+	end
+
+	test "reject post with blank content" do
+		assert_no_difference "Post.count" do
+			Post.create(title: "title", content: "")
 		end
 	end
 end
