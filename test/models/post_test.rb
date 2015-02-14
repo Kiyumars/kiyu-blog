8fa2decb9e2f53b65ae7bfd14f0b2157e13cd85f
Kiyu <qconnection@gmail.com>
2015-02-14 20:29:46 +0100

    Validate title and content of Post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-	test "reject post wit blank title" do
+	test "reject post with blank title" do
 		assert_no_difference "Post.count" do
 			Post.create(title: "", content: "Lorem Ipsum Est")
 		end
@@ -10,6 +10,12 @@ class PostTest < ActiveSupport::TestCase
 	test "reject post with blank content" do
 		assert_no_difference "Post.count" do
 			Post.create(title: "title", content: "")
+		end
+	end
+
+	test "submit valid post" do
+		assert_difference "Post.count", 1 do
+			Post.create(title: "title", content: "Lorem Ipsum Est")
 		end
 	end
 end
