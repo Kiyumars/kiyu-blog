7ae2376e851df4e48d6fa1c10668464a5df9a4e0
Kiyu <qconnection@gmail.com>
2015-02-15 13:16:49 +0100

    Add show action and view for admin/posts controller

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -41,6 +41,10 @@ class Admin::PostsController < ApplicationController
 		end
 	end
 
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	private
 		def safe_post_params
 			params.require(:post).permit(:title, :content)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   resources :posts, only: [:index, :show]
 
   namespace :admin do
-    resources :posts, except: [:show]
+    resources :posts
   end
 end
